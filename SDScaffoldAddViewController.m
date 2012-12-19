659fe22073abf53dd173fdbc7576a5a8c8467580
Steve Derico <steve@stevederico.com>
2012-12-18 17:47:43 -0800

    Working Add Function

--- SDScaffoldAddViewController.m
+++ SDScaffoldAddViewController.m
@@ -11,6 +11,7 @@
 @interface SDScaffoldAddViewController  (){
 
     NSArray *attributes;
+    NSMutableArray *attributeTypes;
     NSMutableArray *values;
     
     NSManagedObject *object;
@@ -28,19 +29,21 @@
         self.tableView.allowsSelection = NO;
         self.entityName = entityName;
         self.managedObjectContext = moc;
+        attributeTypes = [[NSMutableArray alloc] init];
         self.title = [NSString stringWithFormat:@"Add %@",self.entityName.capitalizedString];
         
         NSEntityDescription *entityDescription = [NSEntityDescription entityForName:self.entityName inManagedObjectContext:self.managedObjectContext];
         
         for (NSPropertyDescription *property in entityDescription) {
-            NSLog(@"Property %@",property.description);
+            NSLog(@"Property %@",[[[property valueForKey:@"attributeType"] class] description]);
+            [attributeTypes addObject:[property valueForKey:@"attributeType"]];
         }
         attributes = [[entityDescription attributesByName] allKeys];
          NSLog(@"Array %@",attributes);
         values = [[NSMutableArray alloc] init];
         
         for (int i = 0; i<attributes.count; i++) {
-            [values insertObject:[NSNull null] atIndex:0];
+            [values insertObject:@"!" atIndex:0];
         }
         
     }
@@ -52,8 +55,6 @@
 
     UIBarButtonItem *saveButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemSave target:self action:@selector(saveObject)];
     self.navigationItem.rightBarButtonItem = saveButton;
-    
-    
     
 }
 
@@ -98,19 +99,19 @@
 
     ELCTextFieldCell *c = (ELCTextFieldCell*)cell;
     c.rightTextField.text = @"";
-    if ([values objectAtIndex:indexPath.row] == [NSNull null]) {
+    if ([[values objectAtIndex:indexPath.row] isEqualToString:@"!"]) {
         c.rightTextField.placeholder = [[attributes objectAtIndex:indexPath.row] description];
     }else{
         c.rightTextField.text = [[values objectAtIndex:indexPath.row] description];
     }
     
-    UIDatePicker *datePicker = [[UIDatePicker alloc] init];
+    static UIDatePicker *datePicker;
+    datePicker =  [[UIDatePicker alloc] init];
     datePicker.datePickerMode = UIDatePickerModeDateAndTime;
     [datePicker addTarget:self action:@selector(datePickerValueChanged:) forControlEvents:UIControlEventValueChanged];
   
-    
        NSEntityDescription *entityDescription = [NSEntityDescription entityForName:self.entityName inManagedObjectContext:self.managedObjectContext];
-    
+        
     switch ([[[entityDescription attributesByName] valueForKey:[attributes objectAtIndex:indexPath.row]] attributeType]) {
         case 0:
             c.rightTextField.keyboardType = UIKeyboardTypeNumberPad;   
@@ -137,7 +138,7 @@
             c.rightTextField.keyboardType = UIKeyboardTypeDefault;
             break;
         case 800:
-            c.rightTextField.inputView = [[UISwitch alloc] initWithFrame:CGRectZero];
+            c.rightTextField.keyboardType = UIKeyboardTypeNumberPad;
             break;
         case 900:
                 //Add Date Picker
@@ -150,29 +151,18 @@
             c.rightTextField.keyboardType = UIKeyboardTypeNumberPad; 
             break;
     } 
-   
-    
- 
-    
-
-    
 }
 
 - (void)textFieldCell:(ELCTextFieldCell *)inCell updateTextLabelAtIndexPath:(NSIndexPath *)inIndexPath string:(NSString *)inValue{
 
-    NSLog(@"Row %d",inIndexPath.row );
     if ([values count] > 0) {
         [values removeObjectAtIndex:inIndexPath.row];
         [values insertObject:inValue atIndex:inIndexPath.row];
     }else{
-           [values insertObject:inValue atIndex:inIndexPath.row];
+        [values insertObject:inValue atIndex:inIndexPath.row];
     
     }
     
-     NSLog(@"Values %@",[values description]);
-
-    
-
 }
 
 
@@ -187,9 +177,6 @@
     
     [self textFieldCell:cell updateTextLabelAtIndexPath:indexPath string:s.date.description];
     
-    
-    NSLog(@"DATE Picker %d: %@",s.tag, s.date);
-    
     [self.tableView reloadData];
     
 }
@@ -201,6 +188,66 @@
 
 -(void)saveObject{
 
+    NSManagedObject *managedObject = [NSEntityDescription
+                                    insertNewObjectForEntityForName:self.entityName
+                                    inManagedObjectContext:self.managedObjectContext];
+    int i = 0;
+    for (NSString *attribute in attributes) {
+        NSString *stringValue = [[values objectAtIndex:i] description];
+        NSNumber *intValue = [NSNumber numberWithInt:[[values objectAtIndex:i] integerValue]];
+        NSNumber *floatValue = [NSNumber numberWithFloat:[[values objectAtIndex:i] floatValue]];
+        
+        if ([stringValue isEqualToString:@"!"]) {
+          i++;
+        }else{
+
+            switch ([[attributeTypes objectAtIndex:i] integerValue]) {
+                case 0:
+                    [managedObject setValue:intValue forKey:attribute];
+                    break;
+                case 100:
+                     [managedObject setValue:intValue forKey:attribute];
+                    break;
+                case 200:
+                     [managedObject setValue:intValue forKey:attribute];
+                    break;
+                case 300:
+                     [managedObject setValue:intValue forKey:attribute];
+                    break;
+                case 400:
+                     [managedObject setValue:floatValue forKey:attribute];
+                    break;
+                case 500:
+                     [managedObject setValue:floatValue forKey:attribute];
+                    break;
+                case 600:
+                    [managedObject setValue:floatValue forKey:attribute];
+                    break;
+                case 700:
+                     [managedObject setValue:stringValue forKey:attribute];
+                    break;
+                case 800:
+                     [managedObject setValue:intValue forKey:attribute];
+                    break;
+                case 900:
+                        //Add Date Picker
+//                        [managedObject setValue:intValue forKey:attribute];
+                    
+                    break;
+                    
+                default:
+                    [managedObject setValue:stringValue forKey:attribute];
+                    break;
+            }
+            
+            i++;
+        }
+        
+        
+        
+    }
+    
+    [self.managedObjectContext save:nil];
 
 
 }
