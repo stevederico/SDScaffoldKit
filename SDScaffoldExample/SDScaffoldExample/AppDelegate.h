4bb553540db9bd9907fa84c9ee132ea1499f66c0
Steve Derico <steve@stevederico.com>
2012-12-19 09:11:36 -0800

    Added Example

---
+++ SDScaffoldExample/SDScaffoldExample/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  SDScaffoldExample
+//
+//  Created by Steve Derico on 12/19/12.
+//  Copyright (c) 2012 Bixby Apps. All rights reserved.
+//
+#import "SDScaffoldViewController.h"
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ SDScaffoldExample/SDScaffoldExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SDScaffoldExample
+//
+//  Created by Steve Derico on 12/19/12.
+//  Copyright (c) 2012 Bixby Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
